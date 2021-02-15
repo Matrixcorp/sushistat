a20ee836f8db974fd46e6daf73f739c07ffd7869
Matrixcorp <grigorijkod@gmail.com>
2021-02-15 15:19:35 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { SidebarComponent } from './layouts/sidebar/sidebar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { DeliveryComponent } from './pages/delivery/delivery.component';
+import { DiscountsComponent } from './pages/discounts/discounts.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { HomeComponent } from './pages/home/home.component';
     HeaderComponent,
     SidebarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    MainPageComponent,
+    AboutUsComponent,
+    DeliveryComponent,
+    DiscountsComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
